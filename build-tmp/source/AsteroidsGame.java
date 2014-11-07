6991b5da7876373e9d10b89653d10c55d3c9092e
shleewhite <lwhite@sportsbasement.com>
2014-11-07 11:10:41 -0800

    made ship and stars

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip spaceship;
 Stars sky[];
 public void setup() 
@@ -23,8 +39,8 @@ public void draw()
 }
 public void keyPressed()
   {
-    if (keyCode == UP){spaceship.accelerate(0.3);}
-    else if (keyCode == DOWN){spaceship.accelerate(-0.3);}
+    if (keyCode == UP){spaceship.accelerate(0.3f);}
+    else if (keyCode == DOWN){spaceship.accelerate(-0.3f);}
     else if (keyCode == LEFT){spaceship.rotate(10);}
     else if (keyCode == RIGHT){spaceship.rotate(-10);}
   }
@@ -52,7 +68,7 @@ class SpaceShip extends Floater
       yCorners[6] = 3;
       xCorners[7] = -1;
       yCorners[7] = 6;
-    myColor = #196CE8;
+    myColor = 0xff196CE8;
     myCenterX = 200;
     myCenterY = 200;
     myPointDirection = 0;
@@ -99,7 +115,7 @@ class Stars
   public void show()
   {
     noFill();
-    strokeWeight(0.5);
+    strokeWeight(0.5f);
     stroke(myColor);
     ellipse((int)myX, (int)myY, 4, 4);
     
@@ -182,3 +198,12 @@ abstract class Floater
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
